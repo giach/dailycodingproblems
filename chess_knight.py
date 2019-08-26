3fe46d468131447e36c29aae99aa16b20fee02ab
Georgiana Chelu <georgiana.chelu93@gmail.com>
2019-08-26 22:25:36 +0300

    Knight Shortest Path problem solved with BFS

--- chess_knight.py
+++ chess_knight.py
@@ -1,8 +1,27 @@
+class Queue:
+	def __init__(self):
+		self.size = 0
+		self.queue = []
+
+	def enqueue(self, elem):
+		self.size += 1
+		self.queue += [elem]
+
+	def dequeue(self):
+		first = self.queue[0]
+		self.queue = self.queue[1:]
+		self.size -= 1
+
+		return first
+
+	def isEmpty(self):
+		return self.size == 0
+
 class ChessKnight:
 
-	def __init__(self):
-		self.size = 8
-		self.board = [[0 for j in range(0,self.size)] for i in range(0,self.size)]
+	def __init__(self, n, m):
+		self.size = (n, m)
+		self.board = [[0 for j in range(0,m)] for i in range(0,n)]
 
 	def get_board(self):
 		return self.board
@@ -12,9 +31,9 @@ class ChessKnight:
 
 	def is_position_in(self, pos):
 		x, y = pos
-		if x < 0 or x >= self.size:
+		if x < 0 or x >= self.size[0]:
 			return False
-		if y < 0 or y >= self.size:
+		if y < 0 or y >= self.size[1]:
 			return False
 
 		return True
@@ -33,42 +52,45 @@ class ChessKnight:
 		return [f1, f2, f3, f4, f5, f6, f7, f8]
 
 
-	def find_path(self, c, t, board):
-		if c == t:
-			return 1
-
-		cx, cy = c
-
-		# don't follow a path is the cell is known
-		if board[cx][cy] == 1:
-			return 0
-
-		fn = self.get_functions()
-
-		# mark current cell as known
-		board[cx][cy] = 1
+	def find_path(self, t, board, visited):
 
 		# compute all possible positions
-		for f in fn:
-			new_pos = f(cx, cy)
-			if self.is_position_in(new_pos):
-				print(new_pos)
-				self.find_path(new_pos, t, board)
-			else:
-				return 0
+		fn = self.get_functions()
+
+		while not visited.isEmpty():
+			(cx, cy), dist = visited.dequeue()
+			# visited = visited[1:]
+			if (cx, cy) == t:
+				return dist
+			for f in fn:
+				nx, ny = new_pos = f(cx, cy)
+				if self.is_position_in(new_pos) and board[nx][ny] == 0:
+					visited.enqueue((new_pos, dist + 1)) # += [(new_pos, dist + 1)]
+					board[nx][ny] = 1
+
+		return -1
 
 
 	def is_reachable(self, curr, target):
-		board = [[0 for j in range(0,self.size)] for i in range(0,self.size)]
-		result = self.find_path(curr, target, board)
+		board = self.get_board()
+		visited = Queue()
+		visited.enqueue((curr,0))
+		board[curr[0]][curr[1]] = 1
+		result = self.find_path(target, board, visited)
 		print(result)
 
 def main():
-	knight = ChessKnight()
-	print(knight.get_size())
-	print(knight.get_board())
+	knight1 = ChessKnight(4, 7)
+	# 2, 6 ; 2, 4 -> 2
+	knight1.is_reachable((1,5), (1,3))
 
-	knight.is_reachable((5,5), (3,6))
+	# 0
+	knight2 = ChessKnight(1, 1)
+	knight2.is_reachable((0,0), (0,0))
+
+	# 6
+	knight3 = ChessKnight(8, 8)
+	knight3.is_reachable((0,0),(7,7))
 
 
 
